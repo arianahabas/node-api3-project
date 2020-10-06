6980bc8a471b23013079c860b8a58269cf0ced29
Ariana Habas <65680650+arianahabas@users.noreply.github.com>
2020-10-06 17:04:14 -0500

    logger created

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code away!
+const express = require('express')
+const logger = require('./middleware/logger')
+const postRouter = require('./posts/postRouter')
+
+const server = express()
+const port = 4001
+
+server.use(express.json())
+
+server.use(logger())
+server.use(postRouter)
+
+
+server.listen(port, ()=> {
+    console.log(`Server running at http://localhost:${port}`)
+})
+
+
+
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 router.get('/:id', (req, res) => {
